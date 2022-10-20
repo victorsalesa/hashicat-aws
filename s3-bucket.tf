37849dcacb40c7cdde2cd9bd218186b91ed92676
Victor Salesa <victor.salesasanz@zurich.com>
2022-10-20 19:37:20 +0000

    Corrected s3 bucket module bucket_prefix

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/victorsalesasanz-zurich/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket-prefix = var.prefix
+  bucket_prefix = var.prefix
 
   bucket = "gaurav-s3-bucket"
   acl    = "private"
